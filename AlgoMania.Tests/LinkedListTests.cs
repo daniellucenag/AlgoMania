921e4de4e458a09e57389ce9852f2489f1b78593
Daniel Lucena <daniellucenag@gmail.com>
2023-09-07 14:22:37 -0300

    Adicionado codigo inicial da aula LinkedLists

---
+++ AlgoMania.Tests/LinkedListTests.cs
@@ -0,0 +1,65 @@
+namespace AlgoMania.Tests
+{
+    public class LinkedListTests
+    {
+        [Fact]
+        public void InsertFirstNodeToTail()
+        {
+            var linkedList = new LinkedList();
+            linkedList.InsertNodeToTail(new Node("tail"));
+            Assert.Equal("tail", linkedList.GetTail().Value);
+        }
+
+        [Fact]
+        public void InsertFirstNodeToHead()
+        {
+            var linkedList = new LinkedList();
+            linkedList.InsertNodeToHead(new Node("head"));
+            Assert.Equal("head", linkedList.GetHead().Value);
+        }
+
+        [Fact]
+        public void InsertTwoNodesToHead()
+        {
+            var linkedList = new LinkedList();
+            linkedList.InsertNodeToHead(new Node("head2"));
+            linkedList.InsertNodeToHead(new Node("head1"));
+            Assert.Equal("head1", linkedList.GetHead().Value);
+        }
+
+        [Fact]
+        public void InsertTwoNodesToTail()
+        {
+            var linkedList = new LinkedList();
+            linkedList.InsertNodeToTail(new Node("tail2"));
+            linkedList.InsertNodeToTail(new Node("tail1"));
+            Assert.Equal("tail1", linkedList.GetTail().Value);
+        }
+
+        [Fact]
+        public void InsertNodesToHeadAndTail()
+        {
+            var linkedList = new LinkedList();
+            linkedList.InsertNodeToHead(new Node("head"));
+            linkedList.InsertNodeToTail(new Node("tail"));
+            Assert.Equal("head", linkedList.GetHead().Value);
+            Assert.Equal("tail", linkedList.GetTail().Value);
+        }
+
+        [Fact]
+        public void IsEmptyWithEmptyLinkedList()
+        {
+            var linkedList = new LinkedList();
+            Assert.True(linkedList.IsEmpty());
+        }
+
+        [Fact]
+        public void IsEmptyWithTwoNodes()
+        {
+            var linkedList = new LinkedList();
+            linkedList.InsertNodeToHead(new Node("element"));
+            linkedList.InsertNodeToHead(new Node("element2"));   
+            Assert.False(linkedList.IsEmpty());
+        }
+    }
+}
---
+++ AlgoMania/Program.cs
@@ -0,0 +1,69 @@
+﻿public class Node
+{
+    public Node(string value)
+    {
+        Next = null;
+        Value = value;
+    }
+    public Node Next { get; set; }
+    public string Value { get; set; }
+}
+
+public class LinkedList
+{
+    public Node Head { get; set; }
+    public LinkedList()
+    {
+        Head = new Node("");
+    }
+    
+    public void InsertNodeToHead(Node node)
+    {
+        if (Head.Next != null)
+        {
+            Node tmp = Head.Next;
+            node.Next = tmp;
+            Head.Next = node;
+        }
+        Head.Next = node;
+    }
+
+
+    public void InsertNodeToTail(Node node)
+    {
+        GetTail().Next = node;
+    }
+
+    public Node GetTail()
+    {
+        Node current = this.Head;
+        while (current.Next != null)
+        {
+            current = current.Next;
+        }
+
+        return current;
+    }
+
+    public Node GetHead()
+    {
+        return Head.Next;
+    }
+
+    public bool IsEmpty()
+    {
+        return Head.Next == null;
+    }
+}
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
+
+
